998e2689a4d689dddd8b9db5a0916d9b6d8ff49e
abhishek-2k23 <kumar.abhishek2k23@gmail.com>
2026-06-17 18:13:26 +0530

    fix: cors error

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -4,6 +4,19 @@ import { withSentryConfig } from "@sentry/nextjs";
 const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin-allow-popups",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default withSentryConfig(nextConfig, {
